70e791e9ed9f11e64f77032a97a51412e5ecdbb2
irfan-mohiuddin <irfan95b@gmail.com>
2023-01-06 14:30:49 +0530

    first commit invalid variables

---
+++ Assignment-01/01.c
@@ -0,0 +1,34 @@
+/*
+ * 01. Which are the fallowing are invalid variable names and why?
+ * 	
+ * 	a)  interestpaid
+ * 	b)  si-int
+ * 	c)  AVERAGE
+ * 	d)  percent.
+ * 	e)  123
+ * 	f)  dist in km
+ * 	g)  ot pay
+ * 	h)  Name
+ * 	i)  FLOAT
+ * 	j)  Data_123
+ * 	k)  _data
+ * 	l)  _123
+ * 	m)  123_
+ * 	n)  _
+ */
+
+
+
+
+/*
+ * Answer :
+ * ------
+ *  In C-Programming variable names can be written only using
+ *	upper case latters
+ *	lower case letters
+ *	numbers along with letters in the bigining of the variable (ex: data123) or with underscore "_" (ex: _123)
+ *	and underscore charecter ( _ )
+ *
+ *
+ *		Here in the above variables ( b, d, e, f, g, m ) are invalid.
+ */
